dafee4634b6ad6f729bd91b56e9b0c582689d386
RicardoTimbre <tzt953647107@icloud.com>
2015-06-16 11:12:08 +0800

    JavaLearning

---
+++ JavaSQL/JavaSQL/src/javasql/Student.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javasql;
+
+public class Student {
+    public String StuNumber;
+    public String StuName;
+    public String StuSexual;
+    public String StuAge;
+    public String StuGrade;
+    public String StuClass;
+    public String StuObject;
+    public static String getStuNumber(){
+        
+    }
+
+
+
+}
